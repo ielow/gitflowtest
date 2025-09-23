5b7c04cd9d03624e3cd3c45551b1c2ff7a8bef55
ielow <pako.adn@gmail.com>
2025-09-23 00:50:05 -0600

    Update ui and update documentation

--- src/app/src/App.tsx
+++ src/app/src/App.tsx
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World!</h1>
+        <h1> Welcome to my app</h1>
+        <p>Cada vez se hace mas complicado programar</p>
+        <button>back to basics</button>
       </header>
     </div>
   );
