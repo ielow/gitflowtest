e86024700da0326de7834b663e52820b2f806b1d
ielow <pako.adn@gmail.com>
2025-09-23 08:28:19 -0600

    Changes on server side

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Run server Run')
 })
+
 app.listen(PORT, () => [
     console.log("application running on: http://localhost:"+PORT)
 ])
